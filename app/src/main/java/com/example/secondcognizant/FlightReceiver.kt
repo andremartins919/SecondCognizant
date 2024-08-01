04da5784883a92602d2d19cbcbbe5ce6af1ffc84
abdul <abdul.tanveer@gmail.com>
2024-08-01 20:08:57 +0530

    receiving custom broadcast

---
+++ app/src/main/java/com/example/secondcognizant/FlightReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.secondcognizant
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class FlightReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+       Log.i("Flight","receive a fight broadcast")
+    }
+}
